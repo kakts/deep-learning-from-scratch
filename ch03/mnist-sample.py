84180fc855c2e726774ec940a4709715bddc7adc
kakts <akutsukeita@gmail.com>
2017-02-02 23:03:12 +0900

    Add neuralnet_mnist.py

--- ch03/mnist-sample.py
+++ ch03/mnist-sample.py
@@ -5,6 +5,9 @@ from dataset.mnist import load_mnist
 # 数分かかる
 #(訓練画像, 訓練ラベル), (テスト画像, テストラベル)
 (x_train, t_train), (x_test, t_test) = \
+    # normalize: 入力画像を0.0-1.0の値に正規化するか
+    # flatten: 入力画像を１次元配列にする
+    
     load_mnist(flatten=True, normalize=False)
 
 # それぞれのデータの形状を出力
